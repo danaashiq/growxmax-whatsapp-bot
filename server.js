5d872f5a943376ce816deba4c01ec148914524eb
danaashiq <saeedamanat2003@gmail.com>
2026-03-12 13:35:02 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,75 @@
-import makeWASocket, { useMultiFileAuthState } from "@whiskeysockets/baileys"
+import makeWASocket, {
+  useMultiFileAuthState,
+  DisconnectReason
+} from "@whiskeysockets/baileys"
+
 import qrcode from "qrcode-terminal"
 import { getSheetAnswer } from "./sheet.js"
 import { askGemini } from "./gemini.js"
 
 async function startBot(){
 
-const { state, saveCreds } = await useMultiFileAuthState("auth")
+  const { state, saveCreds } = await useMultiFileAuthState("auth")
 
-const sock = makeWASocket({
-auth: state
-})
+  const sock = makeWASocket({
+    auth: state,
+    printQRInTerminal: true
+  })
 
-sock.ev.on("creds.update", saveCreds)
+  sock.ev.on("creds.update", saveCreds)
 
-sock.ev.on("connection.update", ({qr}) => {
+  sock.ev.on("connection.update", (update) => {
 
-if(qr){
-console.log("Scan this QR Code")
-qrcode.generate(qr,{small:true})
-}
+    const { connection, lastDisconnect, qr } = update
 
-})
+    if(qr){
+      console.log("Scan this QR Code with WhatsApp")
+      qrcode.generate(qr,{small:true})
+    }
 
-sock.ev.on("messages.upsert", async ({messages}) => {
+    if(connection === "open"){
+      console.log("✅ WhatsApp Bot Connected")
+    }
 
-const msg = messages[0]
+    if(connection === "close"){
 
-if(!msg.message) return
+      const shouldReconnect =
+      lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut
 
-const text =
-msg.message.conversation ||
-msg.message.extendedTextMessage?.text ||
-""
+      console.log("connection closed")
 
-let reply = await getSheetAnswer(text)
+      if(shouldReconnect){
+        startBot()
+      }
 
-if(!reply){
-reply = await askGemini(text)
-}
+    }
 
-await sock.sendMessage(msg.key.remoteJid,{text:reply})
+  })
 
-})
+  sock.ev.on("messages.upsert", async ({messages}) => {
+
+    const msg = messages[0]
+
+    if(!msg.message) return
+
+    const text =
+      msg.message.conversation ||
+      msg.message.extendedTextMessage?.text ||
+      ""
+
+    if(!text) return
+
+    console.log("User:", text)
+
+    let reply = await getSheetAnswer(text)
+
+    if(!reply){
+      reply = await askGemini(text)
+    }
+
+    await sock.sendMessage(msg.key.remoteJid,{ text: reply })
+
+  })
 
 }
 
